e70fb91c139b3c9f67e014b2fe18a472e944e53b
christine-george <christineg@rewiringamerica.org>
2025-06-02 09:27:27 -0400

    cleanup

--- src/insert_data_into_postgres_episode.py
+++ src/insert_data_into_postgres_episode.py
@@ -1,18 +1,28 @@
+"""
+insert_data_into_postgres_episode.py
+====================================
+
+This script loads episode metdata from `data/episode_metadata.json` into the
+GCP-hosted PostgreSQL table, `csmap.information.episode`.
+
+Usage
+-----
+
+To execute this script, run:
+    python3 src/insert_data_into_postgres_episode.py
+
+"""
+
 import psycopg
 from dotenv import load_dotenv
 import os
 import json
+from typing import Dict, Any, List
 
-load_dotenv()
-
-# Path to your JSON file
+# Path to the JSON file
 json_file_path = "data/episode_metadata.json"
 
-# Load JSON data from file
-with open(json_file_path, "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-
+# All top-level keys in the data
 expected_keys = [
     "id",
     "title",
@@ -40,6 +50,7 @@ expected_keys = [
     "media_content",
 ]
 
+# All JSONB fields in the data
 json_fields = [
     "title_detail",
     "links",
@@ -56,7 +67,20 @@ json_fields = [
 ]
 
 
-def prepare_json_fields(row):
+def prepare_json_fields(row: Dict[str, Any]) -> Dict[str, Any]:
+    """Prepare nested dictionary fields into serialized JSON strings.
+
+    Parameters
+    ----------
+    row : dict of any
+        A dictionary for a single episode's data.
+
+    Returns
+    -------
+    row : dict of any
+        A dictionary for a single episode's data with nested dictionaries
+        serialized into JSON strings.
+    """
     for field in json_fields:
         if field in row and row[field] is not None:
             row[field] = json.dumps(row[field])
@@ -65,66 +89,93 @@ def prepare_json_fields(row):
     return row
 
 
-print("\ndata loaded")
+def write_to_postgres(dsn: str, data: List[Dict[str, Any]]):
+    """Write each episode to Postgres.
 
-dsn = f"host={os.getenv('DB_HOST')} dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASSWORD')} port={os.getenv('DB_PORT')}"
+    Parameters
+    ----------
+    dsn : str
+        A formatted string containing variables to make the Postgres
+        table connection.
+    data : list of dict
+        The episode data to write.
+    """
+    with psycopg.connect(dsn) as conn:
+        print("\nconnection made")
+        with conn.cursor() as cur:
+            insert_query = """
+            INSERT INTO csmap.information.episode (
+                id, title, title_detail, links, link, summary,
+                summary_detail, published, published_parsed,
+                itunes_episodetype, itunes_episode, authors,
+                author, author_detail, image, subtitle, subtitle_detail,
+                content, itunes_duration, guidislink, ppg_enclosurelegacy,
+                ppg_enclosuresecure, ppg_canonical, media_content
+            )
+            VALUES (
+                %(id)s, %(title)s, %(title_detail)s, %(links)s, %(link)s,
+                %(summary)s, %(summary_detail)s, %(published)s,
+                %(published_parsed)s, %(itunes_episodetype)s, %(itunes_episode)s,
+                %(authors)s, %(author)s, %(author_detail)s, %(image)s,
+                %(subtitle)s, %(subtitle_detail)s, %(content)s,
+                %(itunes_duration)s, %(guidislink)s, %(ppg_enclosurelegacy)s,
+                %(ppg_enclosuresecure)s, %(ppg_canonical)s, %(media_content)s
+            )
+            ON CONFLICT (id) DO UPDATE SET
+                id = EXCLUDED.id,
+                title = EXCLUDED.title,
+                title_detail = EXCLUDED.title_detail,
+                links = EXCLUDED.links,
+                link = EXCLUDED.link,
+                summary = EXCLUDED.summary,
+                summary_detail = EXCLUDED.summary_detail,
+                published = EXCLUDED.published,
+                published_parsed = EXCLUDED.published_parsed,
+                itunes_episodetype = EXCLUDED.itunes_episodetype,
+                itunes_episode = EXCLUDED.itunes_episode,
+                authors = EXCLUDED.authors,
+                author = EXCLUDED.author,
+                author_detail = EXCLUDED.author_detail,
+                image = EXCLUDED.image,
+                itunes_duration = EXCLUDED.itunes_duration,
+                guidislink = EXCLUDED.guidislink,
+                ppg_enclosurelegacy = EXCLUDED.ppg_enclosurelegacy,
+                ppg_enclosuresecure = EXCLUDED.ppg_enclosuresecure,
+                ppg_canonical = EXCLUDED.ppg_canonical,
+                media_content = EXCLUDED.media_content;
+            """
 
-with psycopg.connect(dsn) as conn:
-    print("\nconnection made")
-    with conn.cursor() as cur:
-        insert_query = """
-        INSERT INTO csmap.information.episode (
-            id, title, title_detail, links, link, summary,
-            summary_detail, published, published_parsed,
-            itunes_episodetype, itunes_episode, authors,
-            author, author_detail, image, subtitle, subtitle_detail,
-            content, itunes_duration, guidislink, ppg_enclosurelegacy,
-            ppg_enclosuresecure, ppg_canonical, media_content
-        )
-        VALUES (
-            %(id)s, %(title)s, %(title_detail)s, %(links)s, %(link)s,
-            %(summary)s, %(summary_detail)s, %(published)s,
-            %(published_parsed)s, %(itunes_episodetype)s, %(itunes_episode)s,
-            %(authors)s, %(author)s, %(author_detail)s, %(image)s,
-            %(subtitle)s, %(subtitle_detail)s, %(content)s,
-            %(itunes_duration)s, %(guidislink)s, %(ppg_enclosurelegacy)s,
-            %(ppg_enclosuresecure)s, %(ppg_canonical)s, %(media_content)s
-        )
-        ON CONFLICT (id) DO UPDATE SET
-            id = EXCLUDED.id,
-            title = EXCLUDED.title,
-            title_detail = EXCLUDED.title_detail,
-            links = EXCLUDED.links,
-            link = EXCLUDED.link,
-            summary = EXCLUDED.summary,
-            summary_detail = EXCLUDED.summary_detail,
-            published = EXCLUDED.published,
-            published_parsed = EXCLUDED.published_parsed,
-            itunes_episodetype = EXCLUDED.itunes_episodetype,
-            itunes_episode = EXCLUDED.itunes_episode,
-            authors = EXCLUDED.authors,
-            author = EXCLUDED.author,
-            author_detail = EXCLUDED.author_detail,
-            image = EXCLUDED.image,
-            itunes_duration = EXCLUDED.itunes_duration,
-            guidislink = EXCLUDED.guidislink,
-            ppg_enclosurelegacy = EXCLUDED.ppg_enclosurelegacy,
-            ppg_enclosuresecure = EXCLUDED.ppg_enclosuresecure,
-            ppg_canonical = EXCLUDED.ppg_canonical,
-            media_content = EXCLUDED.media_content;
-        """
-        for row in data:
-            prepared_row = prepare_json_fields(row)
+            for row in data:
+                # Serialize all nested dictionaries
+                prepared_row = prepare_json_fields(row)
 
-            # Ensure all keys exist in prepared_show dict
-            for key in expected_keys:
-                if key not in prepared_row:
-                    prepared_row[key] = None  # or some appropriate default
+                # Fill in missing keys in some episodes with None
+                for key in expected_keys:
+                    if key not in prepared_row:
+                        prepared_row[key] = None
 
-            for key, val in prepared_row.items():
-                print(key, type(val))
+                # Load the row for table insertion
+                cur.execute(insert_query, prepared_row)
+                print(f"\n{prepared_row.get("id")} inserted or updated")
 
-            cur.execute(insert_query, prepared_row)
-            print(f"\n{prepared_row.get("id")} inserted or updated")
-        conn.commit()
-        print(f"\n{len(data)} episodes inserted or updated successfully.")
+            conn.commit()
+
+
+def main():
+    # Load JSON data from file path
+    with open(json_file_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Structure connection variables for Postgres table (defined in .env)
+    load_dotenv()
+    dsn = f"host={os.getenv('DB_HOST')} dbname={os.getenv('DB_NAME')} user={os.getenv('DB_USER')} password={os.getenv('DB_PASSWORD')} port={os.getenv('DB_PORT')}"
+
+    # Write data to Postgres table
+    print("\nWriting data to Postgres...")
+    write_to_postgres(dsn, data)
+
+    print(f"\n{len(data)} episodes inserted or updated successfully.")
+
+
+if __name__ == "__main__":
+    main()
