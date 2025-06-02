c2d284ec5d0244b70836cd523c580a5c753e6b19
christine-george <christineg@rewiringamerica.org>
2025-06-01 23:56:45 -0400

    sql query

---
+++ query.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT e.title AS episode_title, p.title AS podcast_title
+FROM information.episode e
+JOIN information.show p 
+    ON e.title_detail->>'base' = p.title_detail->>'base'
+JOIN transcript.segmented s
+    ON e.id = s.id
+WHERE e.published BETWEEN ('2024-11-05'::date - INTERVAL '14 days') 
+      AND ('2024-11-05'::date + INTERVAL '14 days')
+  AND (s.text ILIKE '%Trump%' OR s.text ILIKE '%Biden%');
