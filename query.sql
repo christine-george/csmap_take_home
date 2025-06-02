953b6741f5db08501096a155ccf691227764f3ff
christine-george <christineg@rewiringamerica.org>
2025-06-02 00:07:13 -0400

    text filtering

--- query.sql
+++ query.sql
@@ -1,4 +1,6 @@
-SELECT DISTINCT e.title AS episode_title, p.title AS podcast_title
+SELECT DISTINCT e.title AS episode_title, 
+                p.title AS podcast_title, 
+                s.text AS segment_text
 FROM information.episode e
 JOIN information.show p 
     ON e.title_detail->>'base' = p.title_detail->>'base'
@@ -6,4 +8,4 @@ JOIN transcript.segmented s
     ON e.id = s.id
 WHERE e.published BETWEEN ('2024-11-05'::date - INTERVAL '14 days') 
       AND ('2024-11-05'::date + INTERVAL '14 days')
-  AND (s.text ILIKE '%Trump%' OR s.text ILIKE '%Biden%');
+  AND (s.text ~* '\mTrump\M' OR s.text ~* '\mBiden\M');
